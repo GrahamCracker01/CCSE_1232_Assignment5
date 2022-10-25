2781612cbe064ddbb7e4b45db1d22c12a905e7ac
GrahamCracker01 <mrgrahamcracker01@gmail.com>
2022-10-25 14:33:09 -0400

    Initial Commit

--- src/Assignment5A.java
+++ src/Assignment5A.java
@@ -1,2 +1,90 @@
-package PACKAGE_NAME;public class Assignment5A {
-}
+//Class: CSE 1321L
+//Section: J02
+///Term: Fall 2022
+//Instructor: Aarthi Poovalingam
+//Name: Asher Graham
+//Lab#: ...
+
+import java.util.Arrays;
+import java.util.Objects;
+import java.util.Scanner;
+class Assignment5A {
+    public static void main(String[] args){
+
+        //declare scanner and variables
+        Scanner scan = new Scanner(System.in), scan2 = new Scanner(System.in);
+        int intInput, intInput2, intCount = 0;
+        String strInput, strTemp;
+        String[] array = new String[5];
+        boolean boolStop = false;
+
+        //initialize array with blanks and first output
+        Arrays.fill(array, " ");
+        System.out.println("[Top 5 Friend's List]");
+
+        //looping menu
+        while (!boolStop) {
+            System.out.print("What would you like to do?\n1) Enter a friend's name\n2) Replace a friend's name\n3) Display your friends list\n4) Quit\nYour choice: ");
+            intInput = scan.nextInt();
+            System.out.println("");
+            switch(intInput) {
+
+                //add friend to next available index
+                case 1:
+                    if (intCount >= 5) {
+                        System.out.println("Sorry, your friends list is full!\n");
+                        break;
+                    }
+                    System.out.print("Enter a name: ");
+                    strInput = scan2.nextLine();
+                    for (int i = 0; i < 5; i++) {
+                        if (Objects.equals(array[i], strInput)) {
+                            System.out.println("Sorry, they're already on the list!");
+                            break;
+                        }
+                    }
+                    array[intCount] = strInput;
+                    System.out.println("");
+                    intCount++;
+                    break;
+
+                //replace friend
+                case 2:
+                    System.out.println("Enter a name: ");
+                    strInput = scan2.nextLine();
+                    for (int i = 0; i < 5; i++) {
+                        if (Objects.equals(array[i], strInput)) {
+                            System.out.println("Sorry, they're already on the list!");
+                            break;
+                        }
+                    }
+                    System.out.println("Enter an index: ");
+                    intInput2 = scan2.nextInt();
+                    if (intInput2 < 0 || intInput2 > 4) {
+                        System.out.println("Sorry, that’s an invalid command!");
+                        break;
+                    }
+                    strTemp = array[intInput2];
+                    array[intInput2] = strInput;
+                    System.out.println("\n" + strInput + " has replaced " + strTemp + " on your friends list!\n");
+                    break;
+
+                //view friends list
+                case 3:
+                    System.out.println("Friend's List:");
+                    for (int i =0; i <5; i++) {
+                        System.out.println((i + 1) + ") " + array[i]);
+                    }
+                    break;
+
+                //quit
+                case 4:
+                    boolStop = true;
+                    break;
+                default:
+                    System.out.println("Sorry, that’s an invalid command!");
+                    break;
+                }
+            }
+        }
+    }
--- src/Assignment5B.java
+++ src/Assignment5B.java
@@ -1,2 +1,76 @@
-package PACKAGE_NAME;public class Assignment5B {
+//Class: CSE 1321L
+//Section: J02
+///Term: Fall 2022
+//Instructor: Aarthi Poovalingam
+//Name: Asher Graham
+//Lab#: ...
+
+import java.util.Scanner;
+class Assignment5B {
+    public static void main(String[] args){
+
+        //declare scanner and variables
+        Scanner scan = new Scanner(System.in);
+        String[][] array = new String[12][31];
+        int intMonth, intDay;
+
+        //fill arrays
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                if ((i ==0 && j >= 20) || (i == 1 && j < 19)) {
+                    array[i][j] = "Aquarius";
+                }
+                else if (i ==1 || (i == 2 && j < 21)) {
+                    array[i][j] = "Pisces";
+                }
+                else if (i ==2 || (i == 3 && j < 20)) {
+                    array[i][j] = "Aries";
+                }
+                else if (i ==3 || (i == 4 && j < 21)) {
+                    array[i][j] = "Taurus";
+                }
+                else if (i ==4 || (i == 5 && j < 22)) {
+                    array[i][j] = "Gemini";
+                }
+                else if (i ==5 || (i == 6 && j < 23)) {
+                    array[i][j] = "Cancer";
+                }
+                else if (i ==6 || (i == 7 && j < 23)) {
+                    array[i][j] = "Leo";
+                }
+                else if (i ==7 || (i == 8 && j < 23)) {
+                    array[i][j] = "Virgo";
+                }
+                else if (i ==8 || (i == 9 && j < 24)) {
+                    array[i][j] = "Libra";
+                }
+                else if (i ==9 || (i == 10 && j < 22)) {
+                    array[i][j] = "Scorpius";
+                }
+                else if (i ==10 || (i == 11 && j < 22)) {
+                    array[i][j] = "Sagittarius";
+                }
+                else {
+                    array[i][j] = "Aquarius";
+                }
+            }
+        }
+        //initial output and get input
+        System.out.println("[What's your sign?]\nWhat month were you born in? ");
+        intMonth = scan.nextInt() - 1;
+        if (intMonth < 0 || intMonth > 11) {
+            System.out.println("That is not a valid month...");
+        }
+        else {
+            System.out.println("And what day? ");
+            intDay = scan.nextInt() - 1;
+            if (intDay < 0 || intDay > 30) {
+                System.out.println("That is not a valid day...");
+            }
+            else {
+                System.out.println("Your sign is " + array[intMonth][intDay] + "!");
+            }
+        }
+
+    }
 }
